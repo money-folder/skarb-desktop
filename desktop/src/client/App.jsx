f0eafc6a040ee1d6f0fa6fbd9cee1e3db4bfef62
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-04-12 22:53:26 +0200

    some more updates

--- desktop/src/client/App.jsx
+++ desktop/src/client/App.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 
+// stores
+import { useActiveWalletStore } from './stores/active-wallet-store';
+
 // views
 import MainView from './views/main-view';
 import Breadcrumbs from './views/breadcrumbs';
+import WalletView from './views/wallet-view';
 
 const App = () => {
+  const activeWalletId = useActiveWalletStore((store) => store.activeWalletId);
+
+  const renderCurrentView = () => {
+    if (activeWalletId) {
+      return <WalletView activeWalletId={activeWalletId} />;
+    }
+
+    return <MainView />;
+  };
+
   return (
-    <div className="w-full h-screen overflow-hidden flex flex-col">
+    <div className="p-5 w-full h-screen overflow-hidden flex flex-col">
       <Breadcrumbs />
 
-      {/* <WalletsView /> */}
-      {/* <ConnectionPanel /> */}
-
-      <MainView />
+      <div className="flex-grow">{renderCurrentView()}</div>
     </div>
   );
 };
