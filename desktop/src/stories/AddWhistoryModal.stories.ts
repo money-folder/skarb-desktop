161a1273cc9895ca0aca6b803541502ec413d90b
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-04-26 22:00:30 +0200

    fix ts issue in AddWhistoryModal

--- desktop/src/stories/AddWhistoryModal.stories.ts
+++ desktop/src/stories/AddWhistoryModal.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
 
 import AddWhistoryModal from '../client/widgets/add-whistory/AddWhistoryModal';
 
@@ -13,6 +14,7 @@ type Story = StoryObj<typeof meta>;
 
 export const main: Story = {
   args: {
-    close: () => {},
+    walletId: '',
+    close: fn(),
   },
 };
