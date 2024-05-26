ff3bf3f42ea49fced8e96c6c3b00a7e3f5efa864
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-05-26 14:17:45 +0200

    serial autoincrement -> text uuid

--- database/queries/migrations.js
+++ database/queries/migrations.js
@@ -1,39 +1,39 @@
 const createMigrationsTableSQL = `
-  create table migrations (
-    m_id integer primary key autoincrement,
-    m_title integer unique not null,
-    m_createdAt datetime default (datetime('now', 'utc'))
+  CREATE TABLE migrations (
+    m_id TEXT PRIMARY KEY,
+    m_title INTEGER UNIQUE NOT NULL,
+    m_createdAt DATETIME DEFAULT (DATETIME('now', 'utc'))
   );
 
   insert into migrations (m_title) values (1);
 `;
 
 const createWalletsTablesSQL = `
-  create table currencies (
-      c_id integer primary key autoincrement,
-      c_name varchar(255) not null,
-      c_createdAt datetime default (datetime('now', 'utc')),
-      c_updatedAt datetime default (datetime('now', 'utc')),
-      c_deletedAt datetime default null
+  CREATE TABLE currencies (
+      c_id TEXT PRIMARY KEY,
+      c_name TEXT NOT NULL,
+      c_createdAt DATETIME DEFAULT (DATETIME('now', 'utc')),
+      c_updatedAt DATETIME DEFAULT (DATETIME('now', 'utc')),
+      c_deletedAt DATETIME DEFAULT NULL
   );
 
-  create table wallets (
-      w_id integer primary key AUTOINCREMENT,
-      w_name varchar(255) not null,
-      w_currencyId integer not null,
-      w_createdAt datetime default (datetime('now', 'utc')),
-      w_updatedAt datetime default (datetime('now', 'utc')),
-      w_deletedAt datetime default null
+  CREATE TABLE wallets (
+      w_id TEXT PRIMARY KEY,
+      w_name TEXT NOT NULL,
+      w_currencyId INTEGER NOT NULL,
+      w_createdAt DATETIME DEFAULT (DATETIME('now', 'utc')),
+      w_updatedAt DATETIME DEFAULT (DATETIME('now', 'utc')),
+      w_deletedAt DATETIME DEFAULT NULL
   );
 
-  create table wallets_history (
-      wh_id integer primary key AUTOINCREMENT,
-      wh_walletId integer not null,
-      wh_moneyAmount numeric not null,
-      wh_date datetime default (datetime('now', 'utc')),
-      wh_createdAt datetime default (datetime('now', 'utc')),
-      wh_updatedAt datetime default (datetime('now', 'utc')),
-      wh_deletedAt datetime default null
+  CREATE TABLE wallets_history (
+      wh_id TEXT PRIMARY KEY,
+      wh_walletId INTEGER NOT NULL,
+      wh_moneyAmount REAL NOT NULL,
+      wh_date DATETIME DEFAULT (DATETIME('now', 'utc')),
+      wh_createdAt DATETIME DEFAULT (DATETIME('now', 'utc')),
+      wh_updatedAt DATETIME DEFAULT (DATETIME('now', 'utc')),
+      wh_deletedAt DATETIME DEFAULT NULL
   );
 
   insert into migrations (m_title) values (2);
