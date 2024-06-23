cba3ade180c937be2ee22988debff5c3f395796f
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-06-23 11:31:56 +0200

    remove column prefix names from migrations

--- database/queries/migrations.js
+++ database/queries/migrations.js
@@ -1,42 +1,34 @@
 const createMigrationsTableSQL = `
   CREATE TABLE migrations (
-    m_id TEXT PRIMARY KEY,
-    m_title INTEGER UNIQUE NOT NULL,
-    m_createdAt DATETIME DEFAULT (DATETIME('now', 'utc'))
+    id TEXT PRIMARY KEY,
+    title INTEGER UNIQUE NOT NULL,
+    created_at DATETIME DEFAULT (DATETIME('now', 'utc'))
   );
 
-  insert into migrations (m_title) values (1);
+  insert into migrations (title) values (1);
 `;
 
 const createWalletsTablesSQL = `
-  CREATE TABLE currencies (
-      c_id TEXT PRIMARY KEY,
-      c_name TEXT NOT NULL,
-      c_createdAt DATETIME DEFAULT (DATETIME('now', 'utc')),
-      c_updatedAt DATETIME DEFAULT (DATETIME('now', 'utc')),
-      c_deletedAt DATETIME DEFAULT NULL
-  );
-
   CREATE TABLE wallets (
-      w_id TEXT PRIMARY KEY,
-      w_name TEXT NOT NULL,
-      w_currencyId INTEGER NOT NULL,
-      w_createdAt DATETIME DEFAULT (DATETIME('now', 'utc')),
-      w_updatedAt DATETIME DEFAULT (DATETIME('now', 'utc')),
-      w_deletedAt DATETIME DEFAULT NULL
+      id TEXT PRIMARY KEY,
+      name TEXT NOT NULL,
+      currency TEXT NOT NULL,
+      created_at DATETIME DEFAULT (DATETIME('now', 'utc')),
+      updated_at DATETIME DEFAULT (DATETIME('now', 'utc')),
+      deleted_at DATETIME DEFAULT NULL
   );
 
   CREATE TABLE wallets_history (
-      wh_id TEXT PRIMARY KEY,
-      wh_walletId INTEGER NOT NULL,
-      wh_moneyAmount REAL NOT NULL,
-      wh_date DATETIME DEFAULT (DATETIME('now', 'utc')),
-      wh_createdAt DATETIME DEFAULT (DATETIME('now', 'utc')),
-      wh_updatedAt DATETIME DEFAULT (DATETIME('now', 'utc')),
-      wh_deletedAt DATETIME DEFAULT NULL
+      id TEXT PRIMARY KEY,
+      wallet_id INTEGER NOT NULL,
+      money_amount REAL NOT NULL,
+      date DATETIME DEFAULT (DATETIME('now', 'utc')),
+      created_at DATETIME DEFAULT (DATETIME('now', 'utc')),
+      updated_at DATETIME DEFAULT (DATETIME('now', 'utc')),
+      deleted_at DATETIME DEFAULT NULL
   );
 
-  insert into migrations (m_title) values (2);
+  insert into migrations (title) values (2);
 `;
 
 const migrationsMap = {
